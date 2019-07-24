bf4a77c0d0ccb4314de7f3dc1734242ab58db122
Michael da Costa Móra <michael.c.mora@gmail.com>
2019-07-24 18:24:21 -0300

    apenas um comentário no main()

--- src/main/java/br/pucrs/springtst/app/AppApplication.java
+++ src/main/java/br/pucrs/springtst/app/AppApplication.java
@@ -2,7 +2,7 @@ package br.pucrs.springtst.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//ver acionando Openshift
 @SpringBootApplication
 public class AppApplication {
 
